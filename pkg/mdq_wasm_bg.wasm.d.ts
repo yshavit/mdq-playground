4dcd8c09e1d378c3b8f326186ed16353e15f18fd
Neko Null <jerrylu.ms@outlook.com>
2025-03-01 19:34:48 +0800

    chore: add artifact

---
+++ pkg/mdq_wasm_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_mdqoptions_free: (a: number, b: number) => void;
+export const mdqoptions_new: () => number;
+export const mdqoptions_output_format: (a: number) => [number, number];
+export const mdqoptions_set_output_format: (a: number, b: number, c: number) => void;
+export const mdqoptions_set_wrap_width: (a: number, b: number) => void;
+export const mdqoptions_set_quiet: (a: number, b: number) => void;
+export const process_markdown: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const console_log: (a: number, b: number) => void;
+export const start: () => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
